84e7cf2ba9026b9b4401a37ff4a72e1c920ad714
valeriapikul <valeriapikul@yandex.ru>
2022-06-27 21:00:54 +0300

    add js and css

---
+++ app/js/main.js
@@ -0,0 +1,29 @@
+document.querySelector('.todo-form__button').onclick = addElement;
+
+function addElement(event) {
+    event.preventDefault();
+    let giveValue = document.querySelector('.todo-form__input');
+    let div = document.querySelector('.todo__list');
+
+    // проверить не пустой ли ввод
+
+    if (giveValue.value !== '') {
+        let newDiv = document.createElement('div');
+        let newCheckbox = document.createElement('input');
+
+        newDiv.classList.add('todo__list-item');
+        div.appendChild(newDiv);
+        newDiv.textContent = giveValue.value;
+
+        newDiv.prepend(newCheckbox);
+        newCheckbox.setAttribute('type', 'checkbox');
+        newCheckbox.classList.add('newCheckbox');
+
+        giveValue.value = ''; // добавить очистку инпута
+
+        newCheckbox.onclick = () => {
+            newDiv.style.textDecoration = 'line-through';
+            newDiv.style.color = '#4F5565';
+        }
+    }
+}
